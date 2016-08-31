0f569675bc2c65419fe2ab00b31b6dcf14ea9c68
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-08-31 20:56:08 +0300

    task started

---
+++ day1/AdditionalContollers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AdditionalContollers
+{
+    public class HomeController : Controller 
+    {
+        public ActionResult Index()
+        {
+            string data = "hello from additional controller";
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ day1/day1/App_Start/RouteConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace day1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
+               new
+               {
+                   controller = "Home",
+                   action = "Index",
+                   id = UrlParameter.Optional
+               },
+               new[] { "AdditionalContollers" });
+
+
+            routes.MapRoute("", "Public/{controller}/{action}",
+                new { controller = "Home", action = "Index" });
+
+            routes.MapRoute("AnotherRoute", "{controller}/{action}/{id}",
+              new
+              {
+                  controller = "Home",
+                  action = "Index",
+                  id = "DefaultId"
+              },
+              new[] { "AdditionalContollers" });
+
+            routes.MapRoute("Route", "{controller}/{action}/{id}",
+                new
+                {
+                    controller = "Home",
+                    action = "Index",
+                    id = UrlParameter.Optional
+                },
+                new[] { "AdditionalContollers" });
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "AdditionalContollers" }
+            );
+        }
+    }
+}
