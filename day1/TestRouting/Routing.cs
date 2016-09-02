f4c7cb5966159de977c7b97ae34adac1f9bac564
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-09-02 12:11:36 +0300

    started test nothing done

---
+++ day1/TestRouting/Routing.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Machine.Specifications;
+
+namespace TestRouting
+{
+    [Subject("routing")]
+    public class Routing
+    {
+      
+    }
+}
