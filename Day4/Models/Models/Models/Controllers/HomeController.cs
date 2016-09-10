b79b1521f87a677ccfb8f313617886167045ecfb
TatyanaLukashevich <tatyana.lukashevich96@gmail.com>
2016-09-10 09:15:25 +0300

    day4 added

---
+++ Day4/Models/Models/Models/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models.Data;
+using Models.Models;
+
+namespace Models.Controllers
+{
+    public class HomeController : Controller
+    {
+	    private readonly IPersonRepo _repo;
+
+	    public HomeController(IPersonRepo personRepo)
+	    {
+		    _repo = personRepo;
+	    }
+
+        public ActionResult Index(int id = 1)
+        {
+	        var person = _repo.GetAll().First(p => p.PersonId == id);
+			return View(person);
+        }
+
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person model)
+        {
+            return View("Index", model);
+        }
+
+        public ActionResult DisplaySummary(
+            [Bind(Prefix = "HomeAddress")] Address summary)
+        {
+            return View(summary);
+        }
+
+        public ActionResult Names(string[] names)
+        {
+            names = names ?? new string[0];
+            return View(names);
+        }
+
+        public ActionResult Addresses()
+        {
+            IList<Address> addresses = new List<Address>();
+            UpdateModel(addresses);
+            return View(addresses);
+        }
+    }
+}
---
+++ Day4/Models/Models/Models/Models/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Models.Models
+{
+	public class Person
+	{
+		public int PersonId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public DateTime BirthDate { get; set; }
+		public Address HomeAddress { get; set; }
+		public bool IsActive { get; set; }
+		public Role Role { get; set; }
+	}
+
+    //[ModelBinder(typeof(CustomModelBinder))]
+	public class Address
+	{
+		public string Line1 { get; set; }
+		public string Line2 { get; set; }
+		public string City { get; set; }
+		public string PostalCode { get; set; }
+		public string Country { get; set; }
+	}
+
+	public enum Role
+	{
+		Admin,
+		User,
+		Guest
+	}
+}
